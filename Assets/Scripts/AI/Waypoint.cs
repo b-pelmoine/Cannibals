78bb17745ffdef0c715ebd7fc739d02c840fe065
CARTIER Nans <Nans.cartier@formation.enjmin.fr>
2017-03-30 17:50:43 +0200

    Waypoints

---
+++ Assets/Scripts/AI/Waypoint.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Waypoint : MonoBehaviour {
+
+    [System.Serializable]
+    public class Wpoint
+    {
+        public Vector3 position;
+        public List<int> links;
+
+        public Wpoint(Vector3 point)
+        {
+            position = point;
+            links = new List<int>();
+        }
+
+        public int this[int key]
+        {
+            get
+            {
+                return links[key];
+            }
+            set
+            {
+                links[key] = value;
+            }
+        }
+    }
+    public List<Wpoint> points = new List<Wpoint>();
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Add(Vector3 point)
+    {
+        points.Add(new Wpoint(point));
+    }
+
+    public void AddLink(int src, int dst)
+    {
+        if (src < 0 || src >= points.Count || dst < 0 || dst > points.Count || points[src].links.Contains(dst))
+            return;
+        points[src].links.Add(dst);
+    }
+
+    public void Delete(int point)
+    {
+        points.RemoveAt(point);
+        for (int i = 0; i < points.Count; i++)
+        {
+            points[i].links.Remove(point);
+            for (int j = 0; j < points[i].links.Count; j++)
+                if (points[i].links[j] > point)
+                    points[i].links[j]--;
+        }
+    }
+
+    public Vector3 this[int key]
+    {
+        get
+        {
+            return points[key].position;
+        }
+        set
+        {
+            points[key].position=value;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/WaypointEditor.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Waypoint))]
+//[CanEditMultipleObjects]
+public class WaypointEditor : Editor {
+
+    public SerializedProperty waypoint;
+    public SerializedProperty links;
+
+    int selectedPoint = -1;
+
+    bool drag = false;
+    bool changed = false;
+    int startPoint = -1;
+	void OnEnable()
+    {
+        waypoint = serializedObject.FindProperty("points");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        var t = (target as Waypoint);
+        if(selectedPoint>=0 && selectedPoint < t.points.Count)
+        {
+            Vector3 newPosition = EditorGUILayout.Vector3Field("Position", t.points[selectedPoint].position);
+            if (newPosition != t.points[selectedPoint].position)
+            {
+                t.points[selectedPoint].position = newPosition;
+                Repaint();
+            }
+            EditorGUILayout.Space();
+            for(int i = 0; i < t.points[selectedPoint].links.Count; i++)
+            {
+                t.points[selectedPoint].links[i] = EditorGUILayout.IntField(t.points[selectedPoint].links[i]);
+            }
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public void OnSceneGUI()
+    {
+        var t = (target as Waypoint);
+
+        int controlID = GUIUtility.GetControlID(FocusType.Passive);
+
+        int lastControlID;
+        for (int i = 0; i < t.points.Count; i++)
+        {
+            EditorGUI.BeginChangeCheck();
+            Handles.color = Color.blue;
+            lastControlID = GUIUtility.hotControl;
+            Handles.FreeMoveHandle(t.points[i].position, Quaternion.identity, .5f, new Vector3(.5f, .5f, .5f), Handles.SphereCap);
+            Handles.color = Color.white;
+            if (GUIUtility.hotControl != lastControlID)
+            {
+                selectedPoint = i;
+                Repaint();
+            }
+            if (EditorGUI.EndChangeCheck())
+            {
+                changed = true;
+                t.points[i].position = castRay();
+                //t.Update();
+            }
+        }
+
+        switch (Event.current.type)
+        {
+            case EventType.mouseDown:
+                if (Event.current.button == 0)
+                {
+                    GUIUtility.hotControl = controlID;
+                    Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                    RaycastHit hitInfo;
+                    if (Physics.Raycast(worldRay, out hitInfo))
+                    {
+                        changed = true;
+                        t.Add(hitInfo.point);
+                    }
+                    HandleUtility.Repaint();
+                    GUIUtility.hotControl = 0;
+                    selectedPoint = -1;
+                }
+                else if(Event.current.button == 1)
+                {
+                    drag = true;
+                    startPoint = nearestPoint();
+                    GUIUtility.hotControl = controlID;
+                }
+
+                break;
+
+            case EventType.mouseUp:
+                if (drag)
+                {
+                    drag = false;
+                    GUIUtility.hotControl = 0;
+                    selectedPoint = -1;
+                    int point = nearestPoint();
+                    if (point != startPoint)
+                        t.points[startPoint].links.Add(point);
+                    HandleUtility.Repaint();
+                    changed = true;
+                }
+                break;
+
+            case EventType.keyDown:
+                if (Event.current.keyCode == KeyCode.Delete && selectedPoint != -1)
+                {
+                    t.Delete(selectedPoint);
+                    changed = true;
+                    selectedPoint = -1;
+                    Event.current.Use();
+                }
+                break;
+
+            case EventType.mouseDrag:
+                HandleUtility.Repaint();
+                break;
+
+            case EventType.Repaint:
+                for(int i=0; i < t.points.Count;i++) 
+                {
+                    var wpoint = t.points[i];
+                    for(int j= 0;j < wpoint.links.Count;j++)
+                    {
+                        var p = wpoint.links[j];
+                        Handles.color = Color.red;
+                        Vector3 v = t.points[p].position - wpoint.position;
+                        Vector3 arrow_position = wpoint.position + v*0.6f;
+                        Vector3 v2 = Quaternion.Euler(0, 15, 0)*(-v.normalized/2);
+                        Vector3 v3 = Quaternion.Euler(0, -15, 0) * (-v.normalized/2);
+                        Handles.DrawLine(wpoint.position, t.points[p].position);
+                        Handles.color = Color.magenta;
+                        Handles.DrawLine(arrow_position, arrow_position+v2);
+                        Handles.DrawLine(arrow_position, arrow_position+v3);
+                        Handles.color = Color.white;
+                    }
+                }
+                if (drag)
+                    Handles.DrawLine(t.points[startPoint].position, castRay());
+                break;
+
+            default:
+                break;
+        }
+        if (changed)
+        {
+            Undo.RegisterCompleteObjectUndo(target, "Waypoint Changed");
+            changed = false;
+        }
+    }
+
+    Vector3 castRay()
+    {
+        RaycastHit hitInfo;
+        Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        if(Physics.Raycast(worldRay, out hitInfo)){
+            return hitInfo.point;
+        }
+        return worldRay.origin+worldRay.direction*10;
+    }
+
+    int nearestPoint()
+    {
+        int result = -1;
+        var t = (target as Waypoint);
+        for(int i = 0; i < t.points.Count; i++)
+        {
+            if (result == -1 || Vector3.Distance(t.points[i].position, castRay()) < Vector3.Distance(t.points[result].position, castRay()))
+                result = i;
+        }
+        return result;
+    }
+}
