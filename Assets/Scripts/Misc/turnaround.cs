ac20810b5e298cec9c0e3d191a1ebe8fc8608779
Tero <contact.tero@gmail.com>
2017-04-13 11:55:57 +0200

    -ADD : Outline shader

---
+++ Assets/Scripts/Misc/turnaround.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnaround : MonoBehaviour {
+    [Range(0f, 20f)]
+	public float speed =5f;
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, Time.deltaTime*speed);
+	}
+}
