52f2047d6cd736c58b80c303b44b52bca3af5e3a
Tero <contact.tero@gmail.com>
2017-04-27 20:10:42 +0200

    -UPDATE offscreen indicator : show AI agents

--- Assets/Scripts/UI/OffscreenIndicator.cs
+++ Assets/Scripts/UI/OffscreenIndicator.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public struct IndicatorData
+{
+    public IndicatorData(Rect pos,float angle) { _pos = pos; _angle = angle; }
+    public Rect _pos;
+    public float _angle;
+}
+
 public class OffscreenIndicator : MonoBehaviour {
 
     public Texture tex_AI;
@@ -10,34 +17,32 @@ public class OffscreenIndicator : MonoBehaviour {
     public Texture tex_Player;
 
     public Camera mCamera;
-    private Vector2 center;
+    private Vector3 center; // center of the screen
+    private Vector3 bounds;
 
     public GameObject[] AIAgents; //public for tests
-    private List<Vector3> AIOnScreenPositions;
+    private List<IndicatorData> AIOnScreenPositions;
     private GameObject[] Players; // assigned automatically
-    private List<Vector3> PlayersOnScreenPositions;
+    private List<IndicatorData> PlayersOnScreenPositions;
     public GameObject Target; // assigned at runtime / may vary over time
-    private Vector3 TargetOnScreenPosition;
+    private IndicatorData TargetOnScreenPosition;
 
-    private bool showAIAgents;
-    private bool showTarget; // automatique ? offscreen = enabled
+    public bool showAIAgents;
+    public bool showTarget; // automatique ? offscreen = enabled
 
-    Vector3 tempPos = Vector3.zero;
-    Vector3 tempCoordScreen = Vector3.zero;
+    Vector3 screenPos = Vector3.zero;
 
     void Start () {
         //AIAgents = GameObject.FindGameObjectsWithTag("Agent");
         Players = GameObject.FindGameObjectsWithTag("Player");
         if(!mCamera)
             mCamera = Camera.main;
-        center = new Vector2(mCamera.pixelWidth / 2, mCamera.pixelHeight / 2);
+        center = new Vector3(Screen.width, Screen.height, 0)/2;
+        bounds = center * 0.9f;
 
-        AIOnScreenPositions = new List<Vector3>();
-        TargetOnScreenPosition = Vector3.zero;
-        PlayersOnScreenPositions = new List<Vector3>();
-
-        showAIAgents = false;
-        showTarget = false;
+        AIOnScreenPositions = new List<IndicatorData>();
+        TargetOnScreenPosition = new IndicatorData();
+        PlayersOnScreenPositions = new List<IndicatorData>();
 	}
 
     public void triggerAgentIndicator(bool state)
@@ -49,7 +54,7 @@ public class OffscreenIndicator : MonoBehaviour {
         showTarget = state;
     }
 
-    void Update () {/*
+    void Update () {
         if(showAIAgents)
         {
             //loop through AI_Agents
@@ -59,34 +64,58 @@ public class OffscreenIndicator : MonoBehaviour {
                 //jump to the next one if not active + TODO if dead
                 if (!go.activeInHierarchy) continue;
 
-                tempPos = mCamera.WorldToViewportPoint(go.transform.position);
-                tempCoordScreen = Camera.main.WorldToScreenPoint(go.transform.position);
+                screenPos = Camera.main.WorldToScreenPoint(go.transform.position);
 
-                if (tempPos.x > 0f && tempPos.x < 1 && tempPos.y > 0 && tempPos.y < 1)
+                if (screenPos.x > 0f && screenPos.x < Screen.width && screenPos.y > 0 && screenPos.y < Screen.height && screenPos.z > 0)
                 {
                      // on Screen
                 }
                 else // off Screen
                 {
-                    if (tempPos.x > 1)
-                    {
-                        GUI.DrawTexture(new Rect(Screen.width - tex_AI.width, Screen.height - tempCoordScreen.y, iconWidth / 2, iconHeight / 2), EnemySymbol);
-                    }
-                    if (tempPos.x < 0)
-                    {
-                        GUI.DrawTexture(new Rect(0, Screen.height - enemyWorldToScreenPoint[i].y, iconWidth / 2, iconHeight / 2), EnemySymbol);
+                    //translate coordinate
+                    screenPos -= center;
+                    if (screenPos.z < 0) screenPos *= -1;
 
-                    }
-                    if (tempPos.y > 1)
-                    {
-                        GUI.DrawTexture(new Rect(enemyWorldToScreenPoint[i].x, 0, iconWidth / 2, iconHeight / 2), EnemySymbol);
-                    }
-                    if (tempPos.y < 0)
-                    {
-                        GUI.DrawTexture(new Rect(enemyWorldToScreenPoint[i].x, Screen.height - EnemySymbol.height, iconWidth / 2, iconHeight / 2), EnemySymbol);
-                    }
+                    float angle = Mathf.Atan2(screenPos.y, screenPos.x);
+                    angle -= 90 * Mathf.Deg2Rad;
+
+                    float sin = -Mathf.Sin(angle);
+                    float cos = Mathf.Cos(angle);
+
+                    screenPos = center + new Vector3(sin * 150, cos * 150, 0);
+
+                    float m = cos / sin;
+
+                    if (cos > 0)
+                        screenPos = new Vector3(bounds.y / m, -bounds.y, 0);
+                    else
+                        screenPos = new Vector3(-bounds.y / m, bounds.y, 0);
+
+                    if (screenPos.x > bounds.x)
+                        screenPos = new Vector3(bounds.x, -bounds.x*m, 0);
+                    else if(screenPos.x < -bounds.x)
+                        screenPos = new Vector3(-bounds.x, bounds.x*m, 0);
+
+                    //undo translation
+                    screenPos += center;
+
+                    AIOnScreenPositions.Add(new IndicatorData(
+                        new Rect(screenPos.x, screenPos.y, Screen.width/20, Screen.height/20),
+                        angle
+                        ));
                 }
             }
-        }*/
+        }
+    }
+
+    void OnGUI()
+    {
+        if(showAIAgents)
+        {
+            foreach (IndicatorData data in AIOnScreenPositions)
+            {
+                GUI.DrawTexture(data._pos, tex_AI, ScaleMode.ScaleToFit);
+            }
+        }
     }
 }
