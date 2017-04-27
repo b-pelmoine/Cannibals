c3fadd5a2ec090dbb30550d6e846779bbe335256
CONSTANTIN <benoitc@equilibregames.com>
2017-04-27 18:37:05 +0200

    -DEL : J'ai supprimé les fichiers wwise hahaha

---
+++ Assets/Scripts/Sound/ForestAmbianceSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestAmbianceSound : MonoBehaviour {
+
+    public AkGameObj akGameObj;
+
+    public void DisposeTree(object in_cookie, AkCallbackType in_type, object in_info)
+    {
+        Debug.Log(gameObject);
+    }
+}
