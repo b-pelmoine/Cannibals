524249ef199a5ee8f74c738eec73324cdb3e64ea
Tero <contact.tero@gmail.com>
2017-04-28 12:30:13 +0200

    -UPDATE offscreen indicator - Add pulsation and dimension change

--- Assets/Scripts/UI/OffscreenIndicator.cs
+++ Assets/Scripts/UI/OffscreenIndicator.cs
@@ -12,6 +12,12 @@ public struct IndicatorData
 
 public class OffscreenIndicator : MonoBehaviour {
 
+    [Range(0.5f, 1.0f)]
+    public float distanceFromScreenBounds;
+
+    [Range(0.5f, 50.0f)]
+    public float pulseSpeed;
+
     public Texture tex_AI;
     public Texture tex_Target;
     public Texture tex_Player;
@@ -21,27 +27,34 @@ public class OffscreenIndicator : MonoBehaviour {
     private Vector3 bounds;
 
     public GameObject[] AIAgents; //public for tests
+    [Range(0.5f, 2f)]
+    public float AIAgentsSizeMultiplier = 1.0f;
+    public bool pulse_AIAgents = false;
     private List<IndicatorData> AIOnScreenPositions;
-    private GameObject[] Players; // assigned automatically
+    public GameObject[] Players; // assigned automatically
+    [Range(0.5f, 2f)]
+    public float PlayersSizeMultiplier = 1.0f;
+    public bool pulse_Players = false;
     private List<IndicatorData> PlayersOnScreenPositions;
-    public GameObject Target; // assigned at runtime / may vary over time
-    private IndicatorData TargetOnScreenPosition;
+    public GameObject[] Targets; // assigned at runtime / may vary over time
+    [Range(0.5f, 2f)]
+    public float TargetsSizeMultiplier = 1.0f;
+    public bool pulse_Targets = false;
+    private List<IndicatorData> TargetOnScreenPositions;
 
     public bool showAIAgents;
     public bool showTarget; // automatique ? offscreen = enabled
+    public bool showPlayers;
 
     Vector3 screenPos = Vector3.zero;
 
     void Start () {
-        //AIAgents = GameObject.FindGameObjectsWithTag("Agent");
-        Players = GameObject.FindGameObjectsWithTag("Player");
         if(!mCamera)
             mCamera = Camera.main;
         center = new Vector3(Screen.width, Screen.height, 0)/2;
-        bounds = center * 0.9f;
 
         AIOnScreenPositions = new List<IndicatorData>();
-        TargetOnScreenPosition = new IndicatorData();
+        TargetOnScreenPositions = new List<IndicatorData>();
         PlayersOnScreenPositions = new List<IndicatorData>();
 	}
 
@@ -54,67 +67,129 @@ public class OffscreenIndicator : MonoBehaviour {
         showTarget = state;
     }
 
-    void Update () {
-        if(showAIAgents)
-        {
-            //loop through AI_Agents
-            AIOnScreenPositions.Clear();
-            foreach (GameObject go in AIAgents)
-            {
-                //jump to the next one if not active + TODO if dead
-                if (!go.activeInHierarchy) continue;
-
-                screenPos = Camera.main.WorldToScreenPoint(go.transform.position);
-
-                if (screenPos.x > 0f && screenPos.x < Screen.width && screenPos.y > 0 && screenPos.y < Screen.height && screenPos.z > 0)
-                {
-                     // on Screen
-                }
-                else // off Screen
-                {
-                    //translate coordinate
-                    screenPos -= center;
-                    if (screenPos.z < 0) screenPos *= -1;
-
-                    float angle = Mathf.Atan2(screenPos.y, screenPos.x);
-                    angle -= 90 * Mathf.Deg2Rad;
-
-                    float sin = -Mathf.Sin(angle);
-                    float cos = Mathf.Cos(angle);
-
-                    screenPos = center + new Vector3(sin * 150, cos * 150, 0);
-
-                    float m = cos / sin;
-
-                    if (cos > 0)
-                        screenPos = new Vector3(bounds.y / m, -bounds.y, 0);
-                    else
-                        screenPos = new Vector3(-bounds.y / m, bounds.y, 0);
-
-                    if (screenPos.x > bounds.x)
-                        screenPos = new Vector3(bounds.x, -bounds.x*m, 0);
-                    else if(screenPos.x < -bounds.x)
-                        screenPos = new Vector3(-bounds.x, bounds.x*m, 0);
-
-                    //undo translation
-                    screenPos += center;
-
-                    AIOnScreenPositions.Add(new IndicatorData(
-                        new Rect(screenPos.x, screenPos.y, Screen.width/20, Screen.height/20),
-                        angle
-                        ));
-                }
-            }
-        }
+    public void triggerPlayersIndicator(bool state)
+    {
+        showPlayers = state;
     }
 
     void OnGUI()
     {
-        if(showAIAgents)
+        if (showAIAgents)
         {
             foreach (IndicatorData data in AIOnScreenPositions)
             {
                 GUI.DrawTexture(data._pos, tex_AI, ScaleMode.ScaleToFit);
+            }
+        }
+        if(showTarget)
+        {
+            foreach (IndicatorData data in TargetOnScreenPositions)
+            {
+                GUI.DrawTexture(data._pos, tex_Target, ScaleMode.ScaleToFit);
+            }
+        }
+        if(showPlayers)
+        {
+            foreach (IndicatorData data in PlayersOnScreenPositions)
+            {
+                GUI.DrawTexture(data._pos, tex_Player, ScaleMode.ScaleToFit);
+            }
+        }
+    }
+
+    void Update () {
+        bounds = center * distanceFromScreenBounds;
+        float pSpeed;
+        if (showAIAgents)
+        {
+            pSpeed = (pulse_AIAgents) ? pulseSpeed : 0;
+            addIndicatorForGameObjects(AIOnScreenPositions, AIAgents, AIAgentsSizeMultiplier, false, pSpeed);
+        }
+        if(showTarget)
+        {
+            pSpeed = (pulse_Targets) ? pulseSpeed : 0;
+            addIndicatorForGameObjects(TargetOnScreenPositions, Targets, TargetsSizeMultiplier, false, pSpeed);
+        }
+        if(showPlayers)
+        {
+            pSpeed = (pulse_Players) ? pulseSpeed : 0;
+            List<GameObject> deadPlayers = new List<GameObject>();
+            foreach(GameObject player in Players)
+            {
+                //check if dead
+                deadPlayers.Add(player);
+            }
+
+            addIndicatorForGameObjects(PlayersOnScreenPositions, deadPlayers.ToArray(), PlayersSizeMultiplier , true, pSpeed);
+        }
+    }
+
+    //add IndicatorData for the given GameObjects into the list
+    void addIndicatorForGameObjects(List<IndicatorData> list, GameObject[] targets, float sizeMultiplier = 1.0f, bool displayOnScreen = false, float pulseSpeed = 0f)
+    {
+        Vector3 playersCenter = Vector3.Lerp(Players[0].transform.position, Players[1].transform.position, .5f);
+        //loop through AI_Agents
+        list.Clear();
+        foreach (GameObject go in targets)
+        {
+            //jump to the next one if not active + TODO if dead
+            if (!go.activeInHierarchy) continue;
+
+            screenPos = Camera.main.WorldToScreenPoint(go.transform.position);
+
+            if (screenPos.x > 0f && screenPos.x < Screen.width && screenPos.y > 0 && screenPos.y < Screen.height && screenPos.z > 0)
+            {
+                if(displayOnScreen)
+                {
+                    Vector2 dimensions = new Vector2(Screen.width / 20, Screen.height / 20) * (sizeMultiplier + 0.1f*(Mathf.Sin(pulseSpeed*Time.time)));
+                    Vector2 position = new Vector2(screenPos.x, (Screen.height-screenPos.y)) - dimensions / 2;
+
+                    list.Add(new IndicatorData(
+                        new Rect(position, dimensions),
+                        0
+                        ));
+                }
+                // on Screen, outline first
+            }
+            else // off Screen
+            {
+                //translate coordinate
+                screenPos -= center;
+                if (screenPos.z < 0) screenPos *= -1;
+
+                float angle = Mathf.Atan2(screenPos.y, screenPos.x);
+                angle -= 90 * Mathf.Deg2Rad;
+
+                float sin = -Mathf.Sin(angle);
+                float cos = Mathf.Cos(angle);
+
+                screenPos = center + new Vector3(sin * 150, cos * 150, 0);
+
+                float m = cos / sin;
+
+                if (cos > 0)
+                    screenPos = new Vector3(bounds.y / m, -bounds.y, 0);
+                else
+                    screenPos = new Vector3(-bounds.y / m, bounds.y, 0);
+
+                if (screenPos.x > bounds.x)
+                    screenPos = new Vector3(bounds.x, -bounds.x * m, 0);
+                else if (screenPos.x < -bounds.x)
+                    screenPos = new Vector3(-bounds.x, bounds.x * m, 0);
+
+                //undo translation
+                screenPos += center;
+
+                float distFromPlayers = Vector3.Distance(playersCenter, go.transform.position);
+
+                Vector2 dimensions = new Vector2(Screen.width / 20, Screen.height / 20) 
+                    * Mathf.Clamp(20 / distFromPlayers, 0.5f, 5) * (sizeMultiplier + 0.1f * (Mathf.Sin(pulseSpeed*Time.time)));
+                Vector2 position = new Vector2(screenPos.x, screenPos.y) - dimensions / 2;
+
+                list.Add(new IndicatorData(
+                    new Rect(position, dimensions),
+                    angle
+                    ));
             }
         }
     }
