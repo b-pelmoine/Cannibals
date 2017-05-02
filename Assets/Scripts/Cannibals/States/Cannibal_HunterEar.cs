fd0da110aa49568c8a3b9fc595550f5b64759c01
CONSTANTIN <benoitc@equilibregames.com>
2017-05-02 13:10:27 +0200

    -ADD : Cannibal_HunterEar pour l'état où le cannibals utilise son ouïe

---
+++ Assets/Scripts/Cannibals/States/Cannibal_HunterEar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannibal_HunterEar : Cannibal_State {
+
+    protected override void OnEnter()
+    {
+        //Mettre Le code de l'ouïe ici
+    }
+
+
+}
