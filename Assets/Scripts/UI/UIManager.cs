a7b5f6c9e65209c836dbb680b6c5fc0cd22b5f97
CARTIER Nans <Nans.cartier@formation.enjmin.fr>
2017-05-02 19:43:20 +0200

    commit

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -8,16 +8,27 @@ public class UIManager : MonoBehaviour {
     public AlterMaterial huntSenseTerrain;
 
 
-    private bool huntSensActive;
+    public bool huntSensActive = false;
     private float elapsedTime;
     [Range(0.2f, 3f)]
     public float transitionDuration;
     [Range(0.1f, 1f)]
     public float HuntSenseFinalIntensity;
+    [Range(2f, 5f)]
+    public float agentSpreadAreaMax;
+
+    private Dictionary<string, bool> cannibalsHuntSenseState;
 
 	// Use this for initialization
 	void Start () {
+<<<<<<< HEAD
         huntSenseTerrain.agentGO = AIAgentManager.getActiveAgents();
+=======
+        huntSenseTerrain.agentGO = IAmanager.getActiveAgents();
+        huntSenseTerrain.effectArea = 0;
+        elapsedTime = 50f;
+        cannibalsHuntSenseState = new Dictionary<string, bool>();
+>>>>>>> 770e679b21308c0344f06787c8ee32c08a22ec6f
     }
 	
 	// Update is called once per frame
@@ -26,38 +37,63 @@ public class UIManager : MonoBehaviour {
         UpdateHuntSense();
 	}
 
-    public void triggerHuntSense(bool state)
+    public void triggerHuntSense(string cannibalID, bool state)
     {
+        cannibalsHuntSenseState[cannibalID] = state;
+        huntSensActive = false;
+        //check if there is at least one cannibal using the huntersense
+        foreach(bool state_t in cannibalsHuntSenseState.Values)
+        {
+            huntSensActive = huntSensActive || state_t;
+        }
         elapsedTime = 0;
-        huntSensActive = state;
-        huntSenseTerrain.effectActive = state;
-        indicator.triggerAgentIndicator(state);
+        indicator.triggerAgentIndicator(huntSensActive);
     }
 
     private void UpdateHuntSense()
     {
         if (huntSensActive)
         {
+            float effectIntensity;
             if (elapsedTime < transitionDuration)
             {
                 elapsedTime += Time.deltaTime;
                 if (elapsedTime > transitionDuration) elapsedTime = transitionDuration;
-                Shader.SetGlobalFloat("_Intensity", (elapsedTime / transitionDuration) * HuntSenseFinalIntensity);
+                effectIntensity = (elapsedTime / transitionDuration) * HuntSenseFinalIntensity;
             }
             else
             {
-                Shader.SetGlobalFloat("_Intensity", HuntSenseFinalIntensity);
+                effectIntensity = HuntSenseFinalIntensity;
             }
             //update the array of AIAgents removing the ones that are dead
+<<<<<<< HEAD
             indicator.AIAgents = AIAgentManager.getActiveAgents().ToArray();
+=======
+            GameObject[] activeAgents = IAmanager.getActiveAgents().ToArray();
+            foreach(GameObject agent in activeAgents)
+            {
+                agent.GetComponent<Renderer>().material.SetFloat("_Intensity", effectIntensity);
+            }
+            huntSenseTerrain.effectArea = agentSpreadAreaMax * effectIntensity * 1/HuntSenseFinalIntensity;
+            //if the list have changed
+            if(activeAgents.Length != indicator.AIAgents.Length)
+                indicator.AIAgents = activeAgents;
+                
+>>>>>>> 770e679b21308c0344f06787c8ee32c08a22ec6f
         }
         else
         {
             if (elapsedTime < transitionDuration)
             {
+                float effectIntensity = ((transitionDuration - elapsedTime) / transitionDuration) * HuntSenseFinalIntensity;
                 elapsedTime += Time.deltaTime;
                 if (elapsedTime > transitionDuration) elapsedTime = transitionDuration;
-                Shader.SetGlobalFloat("_Intensity", ((transitionDuration - elapsedTime) / transitionDuration) * HuntSenseFinalIntensity);
+                GameObject[] activeAgents = IAmanager.getActiveAgents().ToArray();
+                foreach (GameObject agent in activeAgents)
+                {
+                    agent.GetComponent<Renderer>().material.SetFloat("_Intensity", effectIntensity);
+                }
+                huntSenseTerrain.effectArea = agentSpreadAreaMax * effectIntensity * 1 / HuntSenseFinalIntensity;
             }
         }
     }
