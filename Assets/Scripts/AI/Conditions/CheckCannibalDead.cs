a9fa889dc93ef368c83908172230875f99e5ff9c
CARTIER Nans <Nans.cartier@formation.enjmin.fr>
2017-05-03 18:06:35 +0200

    Chasseur+

---
+++ Assets/Scripts/AI/Conditions/CheckCannibalDead.cs
@@ -0,0 +1,30 @@
+﻿using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Name("Check if Cannibal is Dead")]
+[Category("Cannibal")]
+public class CheckCannibalDead : ConditionTask
+{
+    public BBParameter<GameObject> target;
+
+    protected override bool OnCheck()
+    {
+        Cannibal can = target.value.GetComponentInParent<Cannibal>();
+        if (can != null && can.IsDead())
+            return true;
+        else
+            return false;
+    }
+
+    protected override string info
+    {
+        get
+        {
+            return "Cannibal is dead";
+        }
+    }
+
+}
