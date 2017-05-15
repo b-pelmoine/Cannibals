2ff20dd7e034b416884f640237dfdaff9c59c5fd
DESKTOP-POGF7SK\Driiade <benoict@equilibregames.com>
2017-05-15 22:25:15 +0200

    -ADD : refactoring animation -ADD : rotation portée corps en mode solo.

---
+++ Assets/StandardAssets/CustomConditions/FSMStateCondition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+using NodeCanvas.StateMachines;
+using ParadoxNotion.Design;
+
+[Category("FSM")]
+public class FSMStateCondition : ConditionTask {
+
+    public enum Mode { PREVIOUS_STATE, NEXT_STATE}
+
+    public Mode mode;
+    public BBParameter<string> nameState;
+
+    protected override bool OnCheck()
+    {
+        switch(mode)
+        {
+            case Mode.PREVIOUS_STATE: return ((FSM)this.ownerSystem).previousStateName.Equals(nameState);
+            case Mode.NEXT_STATE: return ((FSM)this.ownerSystem).nextState.Equals(nameState);
+        }
+
+        return false;
+    }
+}
