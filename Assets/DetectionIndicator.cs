40f4c4efe763ab4e4c73cc5d8e0794a6fc630ec7
Tero <contact.tero@gmail.com>
2017-05-15 16:03:18 +0200

    -ADD Detection Indicator script and arrow

---
+++ Assets/DetectionIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionIndicator : MonoBehaviour {
+
+    public GameObject[] players;
+    private List<Transform> playerTransforms;
+    private List<GameObject> playerMeshGO;
+
+	void Awake() {
+        //find the player transforms
+        foreach (GameObject go in players)
+            playerTransforms.Add(go.transform.Find("NotStatic").transform);
+        //find the player mesh gameObject owners
+        foreach (Transform t in playerTransforms)
+            playerMeshGO.Add(t.Find("cannibal").gameObject);
+    }
+	
+    public void UpdateTrackersList()
+    {
+
+    }
+	
+}
