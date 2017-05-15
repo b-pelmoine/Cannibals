6d6f0f536a83da872f30909a6175164388a5b63d
DESKTOP-POGF7SK\Driiade <benoict@equilibregames.com>
2017-05-15 08:31:16 +0200

    -ADD : quelques sons cannibal. -MOD : nom du dossier standard Assets car wwise pose problème.

---
+++ Assets/StandardAssets/CustomActions/Wwise_PostEventAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+
+public class Wwise_PostEventAction : ActionTask {
+
+    [RequiredField]
+    public BBParameter<GameObject> gameObject;
+    public string eventName;
+
+    protected override void OnExecute()
+    {
+        base.OnExecute();
+
+        AkSoundEngine.PostEvent(eventName, gameObject.value);
+        EndAction();
+    }
+}
