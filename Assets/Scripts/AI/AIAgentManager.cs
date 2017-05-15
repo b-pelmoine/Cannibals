57876970ef5e3723dd22621d5e82d12a26089eac
CARTIER Nans <Nans.cartier@formation.enjmin.fr>
2017-05-15 16:02:11 +0200

    AgentManager

--- Assets/Scripts/AI/AIAgentManager.cs
+++ Assets/Scripts/AI/AIAgentManager.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public class DetectionData
+{
+    public AI.AIAgent agent;
+    public float detectRate = 0;
+}
+
 public class AIAgentManager : MonoBehaviour {
 
     static public List<GameObject> agents;
+    
+    static Dictionary<GameObject, List<DetectionData>> detected = new Dictionary<GameObject, List<DetectionData>>();
 
 	void Awake () {
         agents = new List<GameObject>();
@@ -22,5 +30,27 @@ public class AIAgentManager : MonoBehaviour {
         return agents;
     }
 
+    static public void addDetectData(GameObject target, DetectionData data)
+    {
+        if (!detected.ContainsKey(target))
+        {
+            detected.Add(target, new List<DetectionData>());
+        }
+        detected[target].Add(data);
+    }
+
+    static public void deleteDetectData(GameObject target, DetectionData data)
+    {
+        detected[target].Remove(data);
+    }
+
+    static public List<DetectionData> getDetectData(GameObject target)
+    {
+        if (!detected.ContainsKey(target))
+        {
+            detected.Add(target, new List<DetectionData>());
+        }
+        return detected[target];
+    }
     
 }
