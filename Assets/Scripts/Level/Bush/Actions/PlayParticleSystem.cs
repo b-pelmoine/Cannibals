9222856f61fcfba6394a37b6eba2458affd986d6
Tero <contact.tero@gmail.com>
2017-05-15 12:30:19 +0200

    -UPDATE main scene adding final Bushed graphic effects
    + particle system play action state

---
+++ Assets/Scripts/Level/Bush/Actions/PlayParticleSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+
+public class PlayParticleSystem : ActionTask {
+
+    public BBParameter<ParticleSystem> leaves;
+
+    protected override void OnUpdate()
+    {
+        leaves.value.Play();
+
+        EndAction();
+    }
+}
