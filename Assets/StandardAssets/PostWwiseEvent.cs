bf0b40ba7a19ec6110bf97fe7e5559edd0c5eb23
CONSTANTIN <benoitc@equilibregames.com>
2017-05-17 17:29:02 +0200

    -MOD : Jouer le son wwise dans l'animation. C'est mieux en fait.

---
+++ Assets/StandardAssets/PostWwiseEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostWwiseEvent : MonoBehaviour {
+
+    public string eventName;
+
+
+    public void PostEvent()
+    {
+        AkSoundEngine.PostEvent(eventName, gameObject);
+    }
+
+
+}
