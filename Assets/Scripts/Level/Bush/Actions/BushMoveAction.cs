20794e5427506fbfaee69114a50bdd37b0ac4ce3
CONSTANTIN <benoitc@equilibregames.com>
2017-05-17 13:36:58 +0200

    -MOD : Les cannibals bougent mieux les buissons suivant leurs actions à l'intérieur.

--- Assets/Scripts/Level/Bush/Actions/BushMoveAction.cs
+++ Assets/Scripts/Level/Bush/Actions/BushMoveAction.cs
@@ -6,6 +6,20 @@ using NodeCanvas.Framework;
 public class BushMoveAction : ActionTask {
 
     public BBParameter<List<Bush>> bushes;
+    public bool playOneTime = false;
+
+    protected override void OnExecute()
+    {
+        base.OnExecute();
+
+        foreach (Bush b in bushes.value)
+        {
+            b.Move();
+        }
+
+        if (playOneTime)
+            EndAction();
+    }
 
     protected override void OnUpdate()
     {
@@ -13,8 +27,6 @@ public class BushMoveAction : ActionTask {
         {
             b.Move();
         }
-
-        EndAction();
     }
 
 }
