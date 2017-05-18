4ae3bce72bd64a35d691d3bb866f5ba3046bd16e
CARTIER Nans <Nans.cartier@formation.enjmin.fr>
2017-05-18 17:09:10 +0200

    Cannibal: Bruit de pas

---
+++ Assets/Scripts/Cannibals/Cannibal_AnimCallback.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannibal_AnimCallback : MonoBehaviour {
+    public GameObject targetObject;
+	void FootSteps()
+    {
+        AkSoundEngine.PostEvent("cannibals_run", targetObject);
+    }
+}
