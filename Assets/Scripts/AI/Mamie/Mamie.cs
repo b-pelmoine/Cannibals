b59242fb48b5f82b452bd2a81bd909eecbac8f10
CARTIER Nans <Nans.cartier@formation.enjmin.fr>
2017-05-18 19:48:23 +0200

    Mamie.

---
+++ Assets/Scripts/AI/Mamie/Mamie.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{
+    public class Mamie : AIAgent {
+        public Waypoint patrouille;
+        Animator animator;
+	    // Use this for initialization
+	    new void Start () {
+            base.Start();
+            animator = GetComponent<Animator>();
+	    }
+	
+	    // Update is called once per frame
+	    new void Update () {
+            base.Update();
+            animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+            if(MoveTo(patrouille.getCurrentDestination(), 3))
+            {
+                patrouille.Next();
+            }
+	    }
+    }
+}
