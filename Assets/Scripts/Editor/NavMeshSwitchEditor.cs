d857a87de26dabd72cb6fdd0c487bb3d4014ee21
CARTIER Nans <Nans.cartier@formation.enjmin.fr>
2017-05-18 12:33:16 +0200

    NavMeshSwitch, pour faire des switchs avec le navmesh (sisi)

---
+++ Assets/Scripts/Editor/NavMeshSwitchEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NavMeshSwitch))]
+public class NavMeshSwitchEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        NavMeshSwitch t = target as NavMeshSwitch;
+        t.switchName = GUILayout.TextField(t.switchName);
+        if (GUILayout.Button("Add"))
+        {
+            t.switchStates.Add(new NavMeshSwitch.SwitchState());
+        }
+        if (GUILayout.Button("Delete"))
+        {
+            t.switchStates.RemoveAt(t.switchStates.Count - 1);
+        }
+
+        foreach(NavMeshSwitch.SwitchState st in t.switchStates)
+        {
+            GUILayout.BeginHorizontal();
+            st.name = EditorGUILayout.TextField("Switch State:",st.name);
+            st.aera = EditorGUILayout.TextField("Aera Name", st.aera);
+            GUILayout.EndHorizontal();
+        }
+
+        serializedObject.ApplyModifiedProperties();
+        SceneView.RepaintAll();
+        PrefabUtility.RecordPrefabInstancePropertyModifications(t);
+    }
+}
---
+++ Assets/Scripts/Sound/NavMeshSwitch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshSwitch : MonoBehaviour {
+    public string switchName = "";
+    [System.Serializable]
+    public class SwitchState
+    {
+        public string name = "";
+        public string aera = "";
+        public int mask = 0;
+    }
+    public List<SwitchState> switchStates = new List<SwitchState>();
+
+    int lastMask = 0;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(switchName);
+		foreach(SwitchState st in switchStates)
+        {
+            st.mask = 1 << NavMesh.GetAreaFromName(st.aera);
+            Debug.Log(st.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NavMeshHit hit;
+        if(NavMesh.SamplePosition(transform.position, out hit, 5, ~0))
+        {
+            if((hit.mask & lastMask) == 0)
+            {
+                Debug.Log("lol3");
+                foreach (SwitchState st in switchStates)
+                    if ((hit.mask & st.mask) != 0)
+                        AkSoundEngine.SetSwitch(switchName, st.name, gameObject);
+            }
+            lastMask = hit.mask;
+        }
+	}
+}
