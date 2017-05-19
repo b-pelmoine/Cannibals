166a467f78977cc86f2b8819ce5679edf6db90e0
Tero <contact.tero@gmail.com>
2017-05-19 17:26:49 +0200

    -MOD camera audio listener now with dynamic position

---
+++ Assets/Scripts/Camera/AudioListenerRelativeToCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioListenerRelativeToCamera : MonoBehaviour {
+
+    private Transform parent;
+    private FollowCam camScript;
+
+    [Range(2f, 20f)]
+    public float factor;
+
+    // Use this for initialization
+    void Start () {
+        parent = transform.parent;
+        camScript = parent.GetComponent<FollowCam>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = camScript.getPlayersBarycenter() + Vector3.up * parent.transform.position.y/factor;
+	}
+}
