35130f9c043da9109dd6ba64057db0805e455c8b
Tero <contact.tero@gmail.com>
2017-05-23 16:47:49 +0200

    -MOD gameScene, baked navmesh & did a few fixes

---
+++ Assets/Scripts/Misc/LineRendererAuto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererAuto : MonoBehaviour {
+
+	void Start()
+    {
+        LineRenderer line = GetComponent<LineRenderer>();
+        List<Vector3> positions = new List<Vector3>();
+        foreach(Transform child in transform)
+        {
+            positions.Add(child.position);
+            Destroy(child.gameObject);
+        }
+        line.positionCount = positions.Count;
+        line.SetPositions(positions.ToArray());
+    }
+}
