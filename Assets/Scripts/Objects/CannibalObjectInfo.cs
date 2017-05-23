477b9ee1b041e3b34d8f137ba078ba556eed9ebb
Tero <contact.tero@gmail.com>
2017-05-23 20:47:55 +0200

    -ADD Object UI

--- Assets/Scripts/Objects/CannibalObjectInfo.cs
+++ Assets/Scripts/Objects/CannibalObjectInfo.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameObjectType
+{
+    NONE,
+    Bottle,
+    Canette,
+    Bone,
+    ChampiBon,
+    ChampiPoison,
+    PlasticDuck,
+    Cookies
+}
+
 [CreateAssetMenu(fileName = "CannibalObjectInfo", menuName ="Cannibal/CannibalObjectInfo")]
 public class CannibalObjectInfo : ScriptableObject {
 
     public Sprite icon;
-
+    public GameObjectType type;
 }
