55b4a4ebe28b2f34667f66a33b2b13c9ebb56aa2
CARTIER Nans <Nans.cartier@formation.enjmin.fr>
2017-05-30 16:42:48 +0200

    Jogger

---
+++ Assets/Scripts/AI/Jogger/Jogger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{
+    public class Jogger : AIAgent {
+        public Waypoint waypoints;
+	    
+	    new void Start () {
+            base.Start();
+            if (waypoints == null || waypoints.points.Count==0)
+            {
+                Debug.LogError("Jogger: Pas de waypoints");
+            }
+            ActionTask root = new ActionTask();
+            root.OnExecute = () =>
+            {
+                if(MoveTo(waypoints.getCurrentDestination(), 3))
+                {
+                    waypoints.Next();
+                }
+            };
+            Play(root);
+	    }
+	
+	    
+    }
+}
