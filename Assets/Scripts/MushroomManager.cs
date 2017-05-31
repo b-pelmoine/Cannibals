6e0056170b7bc8c668d28d161f738a525e758dc8
Sagot Ke-Vinh <kvsagot@gmail.com>
2017-05-31 22:05:03 +0200

    + Mushroom Manager, pour faire repop les champignons

---
+++ Assets/Scripts/MushroomManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomManager : MonoBehaviour {
+
+    [SerializeField]
+    public List<Vector3> champPos = new List<Vector3>();
+
+    public float RefreshRate = 10;
+
+    public GameObject mushroom;
+    public GameObject mushroomHolder;
+
+
+    public LayerMask objectLayer;
+
+	// Use this for initialization
+	void Start () {
+        foreach (GameObject o in GameObject.FindGameObjectsWithTag("Champignon")) {
+            champPos.Add(o.transform.position);
+        }
+        StartCoroutine("RefreshMushroom");
+	}
+
+    IEnumerator RefreshMushroom() {
+        while (true) {
+            foreach (Vector3 v in champPos) {
+                if(!Physics.CheckSphere(v,2, objectLayer)){
+                    GameObject o = Instantiate(mushroom, v, Quaternion.FromToRotation(Vector3.forward,Vector3.up));
+                    o.transform.parent = mushroomHolder.transform;
+                }
+                yield return new WaitForFixedUpdate();
+            }
+            yield return new WaitForSeconds(RefreshRate);
+        }
+    }
+}
